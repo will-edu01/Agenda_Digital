c69d6544462ccb04fb2653fb5bcd67f288c95fda
will-edu01 <patoceifero@gmail.com>
2025-11-18 14:39:27 -0300

    Função de Agendamento concluida

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import home, select_datetime_view, get_available_times, confirm_appointment_view
+from .views import (
+    home,
+    select_datetime_view,
+    get_available_times,
+    confirm_appointment_view,
+    save_appointment_view,
+)
+
 
 urlpatterns = [
     path("", home, name="home"),
     path("selecionar-horario/", select_datetime_view, name="select_datetime"),
     path('horarios/', get_available_times, name='available_times'),
     path("confirmar/", confirm_appointment_view, name="confirm"),
+    path("salvar-agendamento/", save_appointment_view, name="save_appointment"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.http import JsonResponse
 from .utils.schedule import get_available_slots
 from django.shortcuts import render, redirect
-from .models import Service
+from .models import Service, Appointment
 
 
 def home(request):
@@ -79,3 +79,35 @@ def confirm_appointment_view(request):
     }
 
     return render(request, "core/confirm.html", context)
+
+
+def save_appointment_view(request):
+    if request.method != "POST":
+        return redirect("home")
+
+    selected_date = request.POST.get("selected_date")
+    selected_time = request.POST.get("selected_time")
+    service_ids = request.POST.getlist("services")
+
+    services = Service.objects.filter(id__in=service_ids)
+
+    total_minutes = sum(s.duration_minutes for s in services)
+    total_price = sum(s.price for s in services)
+
+    # calcular hora de término
+    from datetime import datetime, timedelta
+    start_dt = datetime.strptime(f"{selected_date} {selected_time}", "%Y-%m-%d %H:%M")
+    end_dt = start_dt + timedelta(minutes=total_minutes)
+
+    # criar agendamento
+    ap = Appointment.objects.create(
+        user=request.user,
+        date=selected_date,
+        start_time=selected_time,
+        end_time=end_dt.time(),
+        total_price=total_price,
+        total_minutes=total_minutes,
+    )
+    ap.services.set(services)
+
+    return render(request, "core/success.html", {"appointment": ap})
