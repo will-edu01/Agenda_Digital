f49f78d5a1c6494cd6244242d087f514df2cae6b
will-edu01 <patoceifero@gmail.com>
2025-12-04 16:40:24 -0300

    Implementação do envio de confirmação por e-mail e melhorias nos JS

---
+++ core/utils/send_email.py
@@ -0,0 +1,23 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_appointment_confirmation(user, services, date, time, total_price):
+    """
+    Envia e-mail de confirmação de agendamento.
+    """
+
+    subject = "🌸 Seu agendamento foi confirmado!"
+    from_email = "SEU_EMAIL_AQUI"
+    to = [user.email]
+
+    html_content = render_to_string("emails/appointment_confirmation.html", {
+        "user_name": user.name or user.email,
+        "services": services,
+        "date": date,
+        "time": time,
+        "total_price": total_price,
+    })
+
+    msg = EmailMultiAlternatives(subject, "", from_email, to)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from .models import Service, Appointment
 from .utils.schedule import get_available_slots
+from .utils.send_email import send_appointment_confirmation
 
 
 
@@ -127,6 +128,14 @@ def save_appointment_api(request):
         total_minutes=total_minutes
     )
     ap.services.set(services)
+
+    send_appointment_confirmation(
+    request.user,
+    services,
+    selected_date,
+    selected_time,
+    total_price
+    )
 
     return JsonResponse({"ok": True, "appointment_id": ap.id})
 
